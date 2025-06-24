afc329aba96d103060aba09a2453588f833677ec
movchan-d <den.a.movchan@gmail.com>
2025-06-24 15:37:31 +0300

    HW2

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,81 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private final Resume[] storage = new Resume[MAX_SIZE];
+    private final static int MAX_SIZE = 10000;
+    private int size;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume" + r.getUuid() + " does not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume" + r.getUuid() + " already exists");
+        } else if (size == MAX_SIZE) {
+            System.out.println("Storage overflow");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " does not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("ERROR");
+        } else {
+//            storage[index] = storage[size - 1];
+            System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
